4976fcb50b9cb2677784fb537630f6443506423c
turbokuzmich <kurteev.d@yandex.ru>
2023-02-26 17:29:09 +0300

    fix

--- pages/api/cdek/calculate.js
+++ pages/api/cdek/calculate.js
@@ -1,7 +1,8 @@
 import get from "lodash/get";
 import { calculate as calculateTariff } from "../../../lib/backend/cdek";
+import cors from "../../../lib/backend/cors";
 
-export default async function calculate(req, res) {
+export default cors(async function (req, res) {
   const code = get(req, "query.code", null);
 
   if (code === null) {
@@ -9,4 +10,4 @@ export default async function calculate(req, res) {
   }
 
   res.status(200).json(await calculateTariff(code));
-}
+});
