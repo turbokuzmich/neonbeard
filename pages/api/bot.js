2b301500dc20fddbb8229fbe6b119f086443a39a
turbokuzmich <kurteev.d@yandex.ru>
2023-03-10 01:31:44 +0300

    bot

--- pages/api/bot.js
+++ pages/api/bot.js
@@ -1,14 +1,44 @@
 import get from "lodash/get";
 import { verify } from "../../lib/helpers/bot";
+import { Order, OrderItem } from "../../lib/backend/sequelize";
 
-export async function bot(req, res) {
-  if (req.method === "POST") {
-    const data = get(req, "body", {});
+const commands = {
+  async listOrders(_, res, data) {
+    const id = get(data, "id", 0);
+    const orders = await Order.suggestById(id);
 
-    console.log(data);
+    return res.status(200).json(orders.map((order) => order.toJSON()));
+  },
+  async viewOrder(_, res, data) {
+    const id = parseInt(get(data, "id", 0), 10);
+    const order = await Order.findByPk(id, {
+      include: [OrderItem],
+    });
 
-    return res.status(200).json({});
+    if (order) {
+      res.status(200).json(order.toJSON());
+    } else {
+      res.status(404).json({});
+    }
+  },
+};
+
+export default async function bot(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({});
   }
 
-  res.status(405).json({});
+  const data = get(req, "body", {});
+
+  if (!verify(data)) {
+    return res.status(403).json({});
+  }
+
+  const command = get(data, "command");
+
+  if (!(command in commands)) {
+    return res.status(400).json({});
+  }
+
+  await commands[command](req, res, data);
 }
