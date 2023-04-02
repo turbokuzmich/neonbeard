2408973569d9e911b2daffa693be7bfe2adfc5d8
turbokuzmich <kurteev.d@yandex.ru>
2023-04-02 16:53:07 +0300

    moysklad integration

--- pages/api/catalog.js
+++ pages/api/catalog.js
@@ -1,6 +1,6 @@
 import cors from "../../lib/backend/cors";
-import { getCachedCatalogItems } from "../../lib/backend/old";
+import { getCatalogItems } from "../../lib/backend/old";
 
 export default async function (_, res) {
-  res.status(200).json(await getCachedCatalogItems());
+  res.status(200).json(await getCatalogItems());
 }
